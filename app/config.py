9753a1a9a39e1ce8a7359db67050c15113ba9030
Francesco Di Leo <francescozn98@gmail.com>
2025-08-07 13:03:49 +0200

    Delete app/config.py

--- app/config.py
+++
@@ -1,3 +0,0 @@
-class Configuration:
-    SECRET_KEY = 'e24fa0737a9d9f32cf18ea7c6f94dd6578b8de8dbd267f845a35b2cccb3640ad'
-    MONGO_URI = "mongodb+srv://admin:hSKEU6pUKFkX.g-@cluster0.8hg36a6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
