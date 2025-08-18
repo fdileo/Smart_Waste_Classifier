33638bf36eb8e3246f8310618ec92bc180144ddc
Francesco Di Leo <francescozn98@gmail.com>
2025-08-18 16:30:36 +0200

    Add files via upload

--- predict_image.py
+++ predict_image.py
@@ -1,17 +1,33 @@
 from keras.models import load_model
 import numpy as np
 
-model = load_model('best_model.h5')
+model = load_model('best_model.h5') # Carico il modello migliore di CNN per la classificazione di rifiuti
 class_names = ['vetro', 'organico', 'carta', 'plastica', 'indifferenziata']
 
 def predict(image, model):
     
-    image = image.convert('RGB')
+    '''
     
-    img = image.resize( (224,224) )
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis = 0)
+    INPUT
+    - **image**: immagine PIL da classificare
+    - **model**: modello di deep_learning addestrato
     
+    OUTPUT
+    - **class_hat**: etichetta della classe predetta
+    - **confidence**: probabilità associata alla classe predetta con 2 cifre decimali
+    
+    '''
+    
+    
+    image = image.convert('RGB')    # Conversione in scala RGB
+    img = image.resize( (224,224) )     # Ridimensiona a 224 x 224 pixel
+    img_array = np.array(img) / 255.0  # Converte l'immagine in array NumPy e normalizza i valori [0,255] → [0,1]
+    
+    # Aggiunge dimensione per essere compatibile con il modello 'best_model.h5'
+    # (224, 224, 3) --> (1, 224, 224, 3) per rappresentare il batch
+    img_array = np.expand_dims(img_array, axis = 0)     
+    
+    # Predizione e probabilità di classificazione
     y_hat_prob = model.predict(img_array)
     y_hat = np.argmax(y_hat_prob)
     class_hat = class_names[y_hat]
